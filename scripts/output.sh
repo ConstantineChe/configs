2c6db8e2cc5c7f5b88fa9d7611961426ad3f5f5f
konstantin.cherkashin <konstantin.cherkashin@symmetrics.de>
2014-10-06 20:26:13 +0300

    refactored i3/conf

---
+++ scripts/output.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if xrandr | grep -q "VGA1 connected"
+then
+    xrandr --output VGA1 --auto --primary --output eDP1 --auto --right-of VGA1
+    i3-msg 'workspace "1: Skype"'
+    i3-msg 'move workspace to output eDP1'
+    i3-msg 'workspace "2: bash"'
+    i3-msg 'move workspace to output eDP1'
+    i3-msg 'workspace "3: Web"'
+    i3-msg 'move workspace to output eDP1'
+    i3-msg 'workspace "4: IDE"'
+    i3-msg 'move workspace to output VGA1'
+    i3-msg 'layout tabbed'
+    i3-msg 'workspace "5: Chromium"'
+    i3-msg 'move workspace to output VGA1'
+    i3-msg 'workspace "6: emacs"'
+    i3-msg 'move workspace to output eDP1'
+else
+    xrandr --auto
+    i3-msg 'workspace "4: IDE"'
+    i3-msg 'layout tabbed'
+fi
