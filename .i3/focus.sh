7adb1a48867450279234597096000d5d25d5185d
konstantin.cherkashin <konstantin.cherkashin@symmetrics.de>
2014-11-05 16:36:24 +0200

    many updates

--- .i3/focus.sh
+++ .i3/focus.sh
@@ -2,12 +2,13 @@
 /usr/bin/i3status --config $1 | (read line && echo $line && read line && echo $line && while :
 do
 	read line
+        mpd=`~/scripts/nowplaying.sh`
 	id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
 	if [ x$id != x ]; then
 		name=$(xprop -id $id | awk '/WM_CLASS/{$1=$2="";print}' | cut -d'"' -f2)
 		name=${name//\\/\\\\}
 		name=${name//\"/\\\"}
-		dat="[{\"name\":\"title\",\"full_text\":\"$name\"},"    
+		dat="[{\"name\":\"title\",\"full_text\":\"$mpd | $name\"},"
 		echo "${line/[/$dat}" || exit 1
 	else
 		echo "$line" || exit 1
---
+++ .i3/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+skype &
+google-chrome-stable &
+chromium &
+emacs &
+phpstorm.sh &
+pycharm &
+thunderbird &
+urxvt -e wicd-curses &
+urxvt &
+urxvt &
+sleep 3 & ~/.i3/output
